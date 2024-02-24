fe0a1a3260bb90b4be388e49e396b1c81e2714fb
Andrey2470T <andreyt2203@gmail.com>
2024-02-24 15:59:20 +0300

    Add ambient light to light color, calculate the color in final_color_blend() if shaders are off

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -387,8 +387,7 @@ class GameGlobalShaderConstantSetter : public IShaderConstantSetter
 		m_animation_timer_delta_pixel{"animationTimerDelta"};
 	CachedPixelShaderSetting<float, 3> m_day_light{"dayLight"};
 	CachedPixelShaderSetting<float, 4> m_star_color{"starColor"};
-	CachedPixelShaderSetting<float, 1> m_ambient_light{"ambientLight"};
-	CachedPixelShaderSetting<float, 3> m_ambient_color{"ambientColor"};
+	CachedPixelShaderSetting<float, 3> m_ambient_light{"ambientLight"};
 	CachedPixelShaderSetting<float, 3> m_eye_position_pixel{"eyePosition"};
 	CachedVertexShaderSetting<float, 3> m_eye_position_vertex{"eyePosition"};
 	CachedPixelShaderSetting<float, 3> m_minimap_yaw{"yawVec"};
@@ -506,20 +505,15 @@ public:
 		float clr[4] = {star_color.r, star_color.g, star_color.b, star_color.a};
 		m_star_color.set(clr, services);
 
-		auto lighting = m_client->getEnv().getLocalPlayer()->getLighting();
+		auto ambient_light = m_client->getEnv().getAmbientLight();
 
-		float ambient_light = lighting.ambient_light.luminance / 16.f;
-
-		m_ambient_light.set(&ambient_light, services);
-
-		video::SColor ambient_color = lighting.ambient_light.color;
-
-		float ac_f[3] = {
-			ambient_color.getRed()/255.f,
-			ambient_color.getGreen()/255.f,
-			ambient_color.getBlue()/255.f
+		float ambient_light_f[3] = {
+			ambient_light.getRed() / 255.f,
+			ambient_light.getGreen() / 255.f,
+			ambient_light.getBlue() / 255.f
 		};
-		m_ambient_color.set(ac_f, services);
+
+		m_ambient_light.set(ambient_light_f, services);
 
 		u32 animation_timer = m_client->getEnv().getFrameTime() % 1000000;
 		float animation_timer_f = (float)animation_timer / 100000.f;
@@ -561,6 +555,8 @@ public:
 
 		m_texel_size0_vertex.set(m_texel_size0_values.data(), services);
 		m_texel_size0_pixel.set(m_texel_size0_values.data(), services);
+
+		auto lighting = m_client->getEnv().getLocalPlayer()->getLighting();
 
 		const AutoExposure &exposure_params = lighting.exposure;
 		std::array<float, 7> exposure_buffer = {
@@ -3520,7 +3516,7 @@ PointedThing Game::updatePointedThing(
 
 		u32 daynight_ratio = client->getEnv().getDayNightRatio();
 		video::SColor c;
-		final_color_blend(&c, light_level, daynight_ratio);
+		final_color_blend(&c, light_level, daynight_ratio, client->getEnv().getAmbientLight());
 
 		// Modify final color a bit with time
 		u32 timer = client->getEnv().getFrameTime() % 5000;
